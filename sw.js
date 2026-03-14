0ca46a656c6acb116b6b77bfee9725199f5fbd45
Nour Eddin <nour.al3nezi@gmail.com>
2026-03-14 14:13:15 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'qr-app-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/easyqrcodejs/dist/easy.qrcode.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// تثبيت ملفات التخزين المؤقت
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// جلب البيانات (العمل بدون إنترنت)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // إذا وجد الملف في الكاش، يعرضه. وإن لم يجده، يجلبه من الإنترنت.
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// تحديث الكاش القديم
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
